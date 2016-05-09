319d4bdd9b64e2cb211582e66c9e4012fae4ee8d
Wu Shi <shiwu.thu10@gmail.com>
2016-05-09 17:50:17 +0800

    Add softmax_dist_layer, select_layer (head file)

---
+++ include/caffe/layers/select_layer.hpp
@@ -0,0 +1,44 @@
+#ifndef CAFFE_SELECT_LAYER_HPP_
+#define CAFFE_SELECT_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Copy a Blob along specified dimensions.
+ */
+template <typename Dtype>
+class SelectLayer : public Layer<Dtype> {
+ public:
+  explicit SelectLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "Select"; }
+  virtual inline int ExactNumBottomBlobs() const { return -1; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  unsigned int num_cand_, outer_dim_, inner_dim_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_SELECT_LAYER_HPP_
